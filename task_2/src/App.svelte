c985a1629db4323ea018d20c364e2db2453666d2
Vladislav Ulyanov <flynnrcore@yandex.ru>
2023-10-09 21:44:14 +0300

    test task complete

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,72 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+  let baseCurrency = 'USD';
+  let targetCurrency = 'RUB';
+  let baseAmount = 1;
+
+  const apiKey = '1da11b167e70b5b8a4fd7ee0';
+
+  let targetAmount = 0;
+
+  function calculate(type) {
+    const url = type === 'base' ? `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${baseCurrency}/${targetCurrency}/${baseAmount}` : `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${targetCurrency}/${baseCurrency}/${targetAmount}`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        if (type === 'base') {
+          targetAmount = data.conversion_result;
+        } else if (type === 'target') {
+          baseAmount = data.conversion_result;
+        }
+      });
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <h1>Конвертер валют</h1>
+
+  <div class="form-controls">
+    <select bind:value={baseCurrency}>
+      <option value="USD">USD</option>
+      <option value="EUR">EUR</option>
+      <option value="RUB">RUB</option>
+    </select>
+
+    <input type="number" bind:value={baseAmount} on:change={() => calculate('base')} />
+
+    <select bind:value={targetCurrency}>
+      <option value="RUB">RUB</option>
+      <option value="USD">USD</option>
+      <option value="EUR">EUR</option>
+    </select>
+
+    <input type="number" bind:value={targetAmount} on:change={() => calculate('target')} />
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .form-controls {
+    display: flex;
+    gap: 10px;
+    align-items: center;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+   select {
+    padding: 8px;
+    font-size: 16px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+  input {
+    padding: 8px;
+    font-size: 16px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
   }
-  .read-the-docs {
-    color: #888;
+
+  input:focus {
+    outline: none;
+    box-shadow: 0 0 4px #66afe9;
+    border-color: #66afe9;
   }
 </style>
