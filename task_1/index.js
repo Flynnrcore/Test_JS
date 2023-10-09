43209ac1d37952183298bba9f06d5158f6c03e2d
Vladislav Ulyanov <flynnrcore@yandex.ru>
2023-10-09 21:45:16 +0300

    task 1 complete

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,31 @@
 import {encoded, translations} from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+const decodeFields = (encoded, translations) => {
+  const excludedFields = ["groupId", "service", "formatSize", "ca"];
 
+  const decoded = encoded.map((item) => {
+    const decodedItem = {};
+  
+    for (let key in item) {
+      if (excludedFields.includes(key)) {
+        decodedItem[key] = item[key];
+      } else if (translations.hasOwnProperty(item[key])) {
+        decodedItem[key] = translations[item[key]];
+      } else {
+        decodedItem[key] = item[key];
+      }
+    }
+  
+    return decodedItem;
+  });
+  
+  const uniqueIds = [...new Set(encoded.flatMap((item) => Object.values(item)).filter((value) => translations.hasOwnProperty(value)))];
+  
+  return { decoded, uniqueIds };
+};
 
+console.log("Let's rock");
+console.log(encoded, translations);
 
-// console.log(decoded)
+const decoded = decodeFields(encoded, translations);
+console.log(decoded);
